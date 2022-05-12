79954cc1ed5c7fc379701a4ba309fc1992fff925
Shylee-bosco <99888687+Shylee-bosco@users.noreply.github.com>
2022-05-12 16:43:47 +0530

    Update adminDetailsAction.js

--- src/redux/actions/adminDetailsAction.js
+++ src/redux/actions/adminDetailsAction.js
@@ -1,4 +1,7 @@
 import axios from "axios"
+import { asyncCourseDetails } from "./CourseDetailsAction"
+import { asyncLectureDetails } from "./LectureDetailsAction"
+import { asyncDetailsStudent } from "./studentDetailsAction"
 
 export const asyncDetailsAdmin = () => {
     return (dispatch) => {
@@ -24,3 +27,50 @@ export const detailsAdmin = (adminResult = {}) => {
         payload: adminResult
     }
 }
+
+// export const asyncLoginAdmin = (formData) => {
+//     return (dispatch) => {
+//         axios.post('https://dct-e-learning-app.herokuapp.com/api/admin/login', formData)
+//         .then((response) => {
+//             const adminResult = response.data
+//             if(adminResult.hasOwnProperty('errors')) {
+//                 alert(adminResult.message)
+//             } else {
+//                 alert(`Successfully Logged in`)
+//                 dispatch(loginAdmin(adminResult))
+//                 localStorage.setItem('token', adminResult.token)
+//                 localStorage.setItem('role', 'admin')
+//                 dispatch(asyncCourseDetails())
+//                 dispatch(asyncDetailsAdmin())
+//                 dispatch(asyncDetailsStudent())
+//                 // dispatch(isAdmin(true));
+//             }
+//         })
+//         .catch((err) => {
+//             console.log(err.message)
+//         })
+//     }
+// }
+
+
+// export const loginAdmin = (adminDetails) => {
+//     return {
+//         type: 'LOGIN_ADMIN',
+//         payload: adminDetails
+//     }
+// }
+
+// export const logoutAdmin = () => {
+//     return {
+//         type: 'LOGOUT_ADMIN'
+//     }
+// }
+// export const asyncAdminLogout =() => {
+//     //remove token
+//     //empty studentdetials
+//     // empty course details
+  
+//     return (dispatch) => {
+//         dispatch(logoutAdmin());
+//     }
+//   }
