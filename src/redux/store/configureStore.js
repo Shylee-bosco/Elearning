ca1fd659ac3ed147e46a013d30a17950483ba5f5
Shylee-bosco <99888687+Shylee-bosco@users.noreply.github.com>
2022-05-17 15:20:14 +0530

    Update configureStore.js

--- src/redux/store/configureStore.js
+++ src/redux/store/configureStore.js
@@ -1,26 +1,26 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
-import registerAdminReducer from '../reducers/registerAdminReducer'
 import thunk from 'redux-thunk'
-import loginAdminReducer from '../reducers/loginAdminReducer'
 import adminDetailsReducer from '../reducers/adminDetailsReducer'
 import registerStudentReducer from '../reducers/registerStudentReducer'
 import studentDetailsReducer from '../reducers/studentDetailsReducer'
-import adminEditReducer from '../reducers/adminEditReducer'
 import coursesDetailsReducer from '../reducers/CousesDetailsReducer'
 import lecturesDetailsReducer from '../reducers/LectureDetailsReducer'
-import commonReducer from '../reducers/commonReducer'
+// import loginAdminReducer from '../reducers/loginAdminReducer'
+// import registerAdminReducer from '../reducers/registerAdminReducer'
+// import adminEditReducer from '../reducers/adminEditReducer'
+// import commonReducer from '../reducers/commonReducer'
 
 const configureStore = () => {
     const store = createStore(combineReducers({
-        registerAdmin: registerAdminReducer,
-        loginAdmin: loginAdminReducer,
         adminDetails: adminDetailsReducer,
         registerStudent : registerStudentReducer,
         studentDetails: studentDetailsReducer,
-        editAdmin: adminEditReducer,
         coursesDetails: coursesDetailsReducer,
         lecturesDetails: lecturesDetailsReducer,
         // isAdmin: commonReducer,
+        // editAdmin: adminEditReducer,
+        // registerAdmin: registerAdminReducer,
+        // loginAdmin: loginAdminReducer,
     }), applyMiddleware(thunk))
     return store
 }
