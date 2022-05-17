517f0a3e18b37faacfb57ea4dada68043d955b0c
Shylee-bosco <99888687+Shylee-bosco@users.noreply.github.com>
2022-05-17 15:13:10 +0530

    Update adminDetailsAction.js

--- src/redux/actions/adminDetailsAction.js
+++ src/redux/actions/adminDetailsAction.js
@@ -28,49 +28,102 @@ export const detailsAdmin = (adminResult = {}) => {
     }
 }
 
-// export const asyncLoginAdmin = (formData) => {
-//     return (dispatch) => {
-//         axios.post('https://dct-e-learning-app.herokuapp.com/api/admin/login', formData)
-//         .then((response) => {
-//             const adminResult = response.data
-//             if(adminResult.hasOwnProperty('errors')) {
-//                 alert(adminResult.message)
-//             } else {
-//                 alert(`Successfully Logged in`)
-//                 dispatch(loginAdmin(adminResult))
-//                 localStorage.setItem('token', adminResult.token)
-//                 localStorage.setItem('role', 'admin')
-//                 dispatch(asyncCourseDetails())
-//                 dispatch(asyncDetailsAdmin())
-//                 dispatch(asyncDetailsStudent())
-//                 // dispatch(isAdmin(true));
-//             }
-//         })
-//         .catch((err) => {
-//             console.log(err.message)
-//         })
-//     }
-// }
+export const asyncLoginAdmin = (formData) => {
+    return (dispatch) => {
+        axios.post('https://dct-e-learning-app.herokuapp.com/api/admin/login', formData)
+        .then((response) => {
+            const adminResult = response.data
+            console.log('adminResult', adminResult)
+            if(adminResult.hasOwnProperty('errors')) {
+                alert(adminResult.message)
+            } else {
+                alert(`Successfully Logged in`)
+                dispatch(loginAdmin(adminResult))
+                localStorage.setItem('token', adminResult.token)
+                localStorage.setItem('role', 'admin')
+                dispatch(asyncCourseDetails())
+                dispatch(asyncDetailsAdmin())
+                dispatch(asyncDetailsStudent())
+                // dispatch(isAdmin(true));
+            }
+        })
+        .catch((err) => {
+            console.log(err.message)
+        })
+    }
+}
 
 
-// export const loginAdmin = (adminDetails) => {
-//     return {
-//         type: 'LOGIN_ADMIN',
-//         payload: adminDetails
-//     }
-// }
+export const loginAdmin = (adminDetails) => {
+    return {
+        type: 'LOGIN_ADMIN',
+        payload: adminDetails
+    }
+}
 
-// export const logoutAdmin = () => {
-//     return {
-//         type: 'LOGOUT_ADMIN'
-//     }
-// }
-// export const asyncAdminLogout =() => {
-//     //remove token
-//     //empty studentdetials
-//     // empty course details
+export const logoutAdmin = () => {
+    return {
+        type: 'LOGOUT_ADMIN'
+    }
+}
+export const asyncAdminLogout =() => {
+    //remove token
+    //empty studentdetials
+    // empty course details
   
-//     return (dispatch) => {
-//         dispatch(logoutAdmin());
+    return (dispatch) => {
+        dispatch(logoutAdmin());
+    }
+  }
+
+export const asyncEditAdmin = (formData) => {
+    return (dispatch) => {
+        axios.put('https://dct-e-learning-app.herokuapp.com/api/admin', formData, {
+                headers: {
+                    'Authorization': localStorage.getItem('token')
+                }
+            })
+            .then((response) => {
+                const adminResult = response.data
+                dispatch(detailsAdmin(adminResult))
+            })
+            .catch((err) => {
+                console.log(err.message)
+            })
+    }
+}
+
+
+// export const editAdmin = (adminResult = {}) => {
+//     return {
+//         type: 'EDIT_ADMIN',
+//         payload: adminResult
 //     }
-//   }
+// }
+
+export const asyncRegisterAdmin = (formData) => {
+    
+    return (dispatch) => {
+        axios.post('https://dct-e-learning-app.herokuapp.com/api/admin/register', formData)
+        .then((response) => {
+            const adminResult = response.data
+            if(adminResult.hasOwnProperty('errors')) {
+                alert(adminResult.message)
+            } else {
+                alert(`Successfully created admin for ${adminResult.academy.name}`)
+                dispatch(setAdmin(adminResult))              
+            }
+       })
+        .catch((err) => {
+            console.log(err.message)
+        })
+    }
+}
+
+
+export const setAdmin = (adminDetails) => {
+    return {
+        type: 'SET_ADMIN',
+        payload: adminDetails
+    }
+}
