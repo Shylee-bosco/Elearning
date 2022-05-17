1d1be4cc2ac0535b43be05c3ecbe272bfa65db70
Shylee-bosco <99888687+Shylee-bosco@users.noreply.github.com>
2022-05-17 15:17:02 +0530

    Update adminDetailsReducer.js

--- src/redux/reducers/adminDetailsReducer.js
+++ src/redux/reducers/adminDetailsReducer.js
@@ -2,15 +2,21 @@ const adminInitialvalue = {}
 
 const adminDetailsReducer = (state = adminInitialvalue, action) => {
     switch (action.type) {
+        case 'SET_ADMIN': {
+            return { ...action.payload }
+        }
         case 'DETAILS_ADMIN': {
+            return { ...action.payload } 
+        }
+        case 'LOGIN_ADMIN': {
+            return { ...action.payload }
+        }
+        case 'LOGOUT_ADMIN': {
+            return adminInitialvalue
+        }
+        case 'EDIT_ADMIN': {
             return {...action.payload} 
         }
-        // case 'LOGIN_ADMIN': {
-        //     return { ...action.payload }
-        // }
-        // case 'LOGOUT_ADMIN': {
-        //     return adminInitialvalue
-        // }
         default: {
             return {...state}
         }
